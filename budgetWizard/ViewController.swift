d5b7b2c4df64086291cfab54505011d55c85591a
Kent <kent_mcnamara@icloud.com>
2019-06-01 20:51:29 +1200

    Initial Commit

---
+++ budgetWizard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  budgetWizard
+//
+//  Created by Kent McNamara on 1/06/19.
+//  Copyright © 2019 Kent McNamara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
