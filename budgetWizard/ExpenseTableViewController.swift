b6b04dd2febac57295d455f16a566f4ad1578fe1
Kent <kent_mcnamara@icloud.com>
2019-06-18 22:07:32 +1200

    restructuring navigation for expense table view

---
+++ budgetWizard/ExpenseTableViewController.swift
@@ -0,0 +1,104 @@
+//
+//  ExpenseTableViewController.swift
+//  budgetWizard
+//
+//  Created by Kent McNamara on 18/06/19.
+//  Copyright © 2019 Kent McNamara. All rights reserved.
+//
+
+import UIKit
+import CoreData
+import os.log
+
+class ExpenseTableViewController: UITableViewController {
+
+    //MARK: Proeprties
+    var expenses = [Expenses]()
+    var expense: Expenses?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getExpenses()
+
+    }
+    
+    //MARK: Get Expenses from DB
+    func getExpenses(){
+        let fetchRequest: NSFetchRequest<Expenses> = Expenses.fetchRequest()
+        do{
+            let expenses = try PersistenceService.context.fetch(fetchRequest)
+            self.expenses = expenses
+        }catch{
+            os_log("Error getting expenses collection from db", log: OSLog.default, type: .error)
+        }
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return expenses.count
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
