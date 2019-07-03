fc7549f7d728f21e54636193652b0a6e6a13c8bd
Kent <kent_mcnamara@icloud.com>
2019-07-03 21:35:27 +1200

    added colour scheme as assets -- tidied up files and put into groups

--- budgetWizard/Summaries/SummaryViewController.swift
+++ budgetWizard/Summaries/SummaryViewController.swift
@@ -19,10 +19,43 @@ class SummaryViewController: UIViewController {
     var testData3 = PieChartDataEntry(value: 0)
     var testData4 = PieChartDataEntry(value: 0)
     var pieChartDataEntries = [PieChartDataEntry]()
+    var test = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        pieChart.chartDescription?.text = ""
+//        pieChart.transparentCircleColor = UIColor.clear
+//        pieChart.holeRadiusPercent = 0
+//        testData1.value = 24.0
+//        testData1.label    = "Test data 1"
+//        testData2.value = 56.4
+//        testData2.label = "Test Data 2"
+//        testData3.value = 78.2
+//        testData3.label = "Test Data 3"
+//        testData4.value = 12.5
+//        testData4.label = "Test Data 4"
+//        pieChart.legend.textColor = UIColor.black
+//
+//        pieChartDataEntries = [testData1, testData2, testData3, testData4]
+//
+//        updateChartData()
+    }
     
+    func updateChartData(){
+        
+        let dataSet = PieChartDataSet(entries: pieChartDataEntries, label: "My Test Chart")
+        let chartData = PieChartData(dataSet: dataSet)
+        
+        //let colours = [UIColor.red, UIColor.green, UIColor.yellow, UIColor.blue]
+        let colours = [UIColor(named: "MyPeach"), UIColor(named: "MyLime"), UIColor(named: "MyRed"), UIColor(named: "MyBlue")]
+        dataSet.colors = colours as! [NSUIColor]
+        
+        pieChart.data = chartData
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
         pieChart.chartDescription?.text = ""
         pieChart.transparentCircleColor = UIColor.clear
         pieChart.holeRadiusPercent = 0
@@ -35,21 +68,14 @@ class SummaryViewController: UIViewController {
         testData4.value = 12.5
         testData4.label = "Test Data 4"
         pieChart.legend.textColor = UIColor.black
-      
+        
         pieChartDataEntries = [testData1, testData2, testData3, testData4]
         
         updateChartData()
     }
     
-    func updateChartData(){
+    override func viewDidAppear(_ animated: Bool) {
         
-        let dataSet = PieChartDataSet(entries: pieChartDataEntries, label: "My Test Chart")
-        let chartData = PieChartData(dataSet: dataSet)
-        
-        let colours = [UIColor.red, UIColor.green, UIColor.yellow, UIColor.blue]
-        dataSet.colors = colours
-        
-        pieChart.data = chartData
     }
 
     /*
